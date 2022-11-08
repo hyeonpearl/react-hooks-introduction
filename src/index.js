dcbd9b314ed0656ac9ac81cada9894af941b3cc9
hyeonpearl <hc9064@gmail.com>
2022-11-08 17:32:40 +0900

    feat : Add useTitle hook

--- src/index.js
+++ src/index.js
@@ -2,33 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { useInput } from './hooks/useInput';
 import { useTabs } from './hooks/useTabs';
+import { useTitle } from './hooks/useTitle';
 
 const App = () => {
-  const content = [
-    {
-      tab: 'Section 1',
-      content: 'Content of Section 1 ',
-    },
-    {
-      tab: 'Section 2',
-      content: 'Content of Section 2 ',
-    },
-  ];
-
-  const { currentItem, changeItem } = useTabs(0, content);
+  const titleUpdater = useTitle('Change Title!');
   return (
     <div>
-      {content.map((section, index) => (
-        <button onClick={() => changeItem(index)}>{section.tab}</button>
-      ))}
-      <div>{currentItem.content}</div>
+      <h1>Hello</h1>
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
