7327ac375824a5464fe341078147a0b6173e71fc
hyeonpearl <hc9064@gmail.com>
2022-11-09 20:53:44 +0900

    -

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { useNotification } from './hooks/useNotification';
 
 const App = () => {
-  const triggerNotice = useNotification('Hello!!!!!', {
-    body: 'nice to meet you!!!',
-  });
   return (
     <div>
-      <button onClick={triggerNotice}>
-        <h1>Hello</h1>
-      </button>
+      <h1>Hello</h1>
     </div>
   );
 };
